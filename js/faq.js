70bec4d94c51787d4eba77cc5e7fb838404f0c96
Eddym07 <eddymartin0701@gmail.com>
2021-10-16 11:38:06 +0200

    FAQ en brut

--- js/faq.js
+++ js/faq.js
@@ -1,4 +1,49 @@
-const dropcont = document.getElementByClassName(dropdown-content);
-dropcont.addEventListener("click", function) {
+let rep1 = document.getElementById("rep1");
+let quest1 = document.getElementById("quest1");
 
-}
+
+
+quest1.addEventListener('click', function() {
+    rep1.classList.toggle("dispnone");
+
+});
+
+let rep2 = document.getElementById("rep2");
+let quest2 = document.getElementById("quest2");
+
+
+
+quest2.addEventListener('click', function() {
+    rep2.classList.toggle("dispnone");
+
+});
+
+let rep3 = document.getElementById("rep3");
+let quest3 = document.getElementById("quest3");
+
+
+
+quest3.addEventListener('click', function() {
+    rep3.classList.toggle("dispnone");
+
+});
+
+let rep4 = document.getElementById("rep4");
+let quest4 = document.getElementById("quest4");
+
+
+
+quest4.addEventListener('click', function() {
+    rep4.classList.toggle("dispnone");
+
+});
+
+let rep5 = document.getElementById("rep5");
+let quest5 = document.getElementById("quest5");
+
+
+
+quest5.addEventListener('click', function() {
+    rep5.classList.toggle("dispnone");
+
+});
