472c0642038d02f81eebd71f6d9253e600ba5722
Anger romain <anger.romain@gmail.com>
2021-10-18 12:21:28 +0200

    test resolution faq

--- js/faq.js
+++ js/faq.js
@@ -1,7 +1,4 @@
-let rep1 = document.getElementById("rep1");
 let quest1 = document.getElementById("quest1");
-
-
 
 quest1.addEventListener('click', function() {
     rep1.classList.toggle("dispnone");
@@ -9,10 +6,7 @@ quest1.addEventListener('click', function() {
     chev1up.classList.toggle("dispnone");
 });
 
-let rep2 = document.getElementById("rep2");
 let quest2 = document.getElementById("quest2");
-
-
 
 quest2.addEventListener('click', function() {
     rep2.classList.toggle("dispnone");
@@ -20,10 +14,7 @@ quest2.addEventListener('click', function() {
     chev2up.classList.toggle("dispnone");
 });
 
-let rep3 = document.getElementById("rep3");
 let quest3 = document.getElementById("quest3");
-
-
 
 quest3.addEventListener('click', function() {
     rep3.classList.toggle("dispnone");
@@ -31,10 +22,7 @@ quest3.addEventListener('click', function() {
     chev3up.classList.toggle("dispnone");
 });
 
-let rep4 = document.getElementById("rep4");
 let quest4 = document.getElementById("quest4");
-
-
 
 quest4.addEventListener('click', function() {
     rep4.classList.toggle("dispnone");
@@ -42,10 +30,7 @@ quest4.addEventListener('click', function() {
     chev4up.classList.toggle("dispnone");
 });
 
-let rep5 = document.getElementById("rep5");
 let quest5 = document.getElementById("quest5");
-
-
 
 quest5.addEventListener('click', function() {
     rep5.classList.toggle("dispnone");
