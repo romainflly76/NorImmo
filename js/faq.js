6f05daf511557a2e78dfd3c33947960639d181d6
Anger romain <55795928+joebar27@users.noreply.github.com>
2021-10-18 13:58:37 +0200

    Update faq.js
    resolution probleme js

--- js/faq.js
+++ js/faq.js
@@ -4,7 +4,7 @@ let quest1 = document.getElementById("quest1");
 quest1.addEventListener('click', function() {
     rep1.classList.toggle("dispnone");
     chev1down.classList.toggle("dispnone");
-    chev1up.classlist.toggle("dispnone");
+    chev1up.classList.toggle("dispnone");
 
 });
 
@@ -14,7 +14,7 @@ let quest2 = document.getElementById("quest2");
 quest2.addEventListener('click', function() {
     rep2.classList.toggle("dispnone");
     chev2down.classList.toggle("dispnone");
-    chev2up.classlist.toggle("dispnone");
+    chev2up.classList.toggle("dispnone");
 
 });
 
@@ -24,7 +24,7 @@ let quest3 = document.getElementById("quest3");
 quest3.addEventListener('click', function() {
     rep3.classList.toggle("dispnone");
     chev3down.classList.toggle("dispnone");
-    chev3up.classlist.toggle("dispnone");
+    chev3up.classList.toggle("dispnone");
 
 });
 
@@ -34,7 +34,7 @@ let quest4 = document.getElementById("quest4");
 quest4.addEventListener('click', function() {
     rep4.classList.toggle("dispnone");
     chev4down.classList.toggle("dispnone");
-    chev4up.classlist.toggle("dispnone");
+    chev4up.classList.toggle("dispnone");
 
 });
 
@@ -44,6 +44,6 @@ let quest5 = document.getElementById("quest5");
 quest5.addEventListener('click', function() {
     rep5.classList.toggle("dispnone");
     chev5down.classList.toggle("dispnone");
-    chev5up.classlist.toggle("dispnone");
+    chev5up.classList.toggle("dispnone");
 
 });
